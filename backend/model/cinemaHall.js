b4bc450bfadc4036dcaece8e7727ac010490b742
AlessandroCanuti <126000158+AlessandroCanuti@users.noreply.github.com>
2024-03-14 16:10:52 +0100

    Create CinemaHall

---
+++ backend/model/cinemaHall.js
@@ -0,0 +1,29 @@
+const{DataTypes, Model} = requeire('sequelize');
+const sequelize = require('../database/db');
+
+class CinemaHall extends Model{}
+
+CinemaHall.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    }
+)
