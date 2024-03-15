7777bf989e85327c19bbebe923994703e0a72bbf
tasso01 <francescotasso16@gmail.com>
2024-03-15 20:07:24 +0100

    database synchronization

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,8 +2,4 @@ const { Sequelize } = require('sequelize');
 const config = require('./config');
 const sequelize = new Sequelize(config);
 
-sequelize.authenticate().then(() => {
-    console.log('Connected to the database.');
-}).catch((error) => {
-    console.error('Unable to connect to the database:', error);
-});
+module.exports = sequelize;
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
+const sequelize = require('./database/connection');
+const associations = require('./models/associations');
 
+sequelize.sync({ force: true });
 
 
 
