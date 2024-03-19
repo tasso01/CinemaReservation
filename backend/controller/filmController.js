1a6107887c5f62f8cbf820e475214f9ca2ed4d23
tasso01 <francescotasso16@gmail.com>
2024-03-19 21:51:21 +0100

    Film Controller

--- backend/controller/filmController.js
+++ backend/controller/filmController.js
@@ -1,15 +1,82 @@
+const Film = require('../models/film');
 
+exports.getAllFilms = async (req, res) => {
+    try {
+        const allFilms = await Film.findAll();
+        return res.status(200).json(allFilms);
+    } catch (error) {
+        return res.status(500).send({error: "Error retrieving films", status: 500});
+    }
+}
 
-//getAllFilms()
+exports.getFilmById = async (req, res) => {
+    try {
+        const film = await Film.findByPk(req.params.id);
+        if (film == null)
+            return res.status(404).send({message: 'Film not found', status: 404});
+        return res.status(200).json(game);
+    } catch (error) {
+        return res.status(500).send({error: 'Error retrieving film', status: 500});
+    }
+}
 
-//getFilmById()
+exports.getFilmByTitle = async (req, res) => {
+    try {
+        const film = await Film.findOne({where: {title: req.params.title}});
+        if (film == null)
+            return res.status(404).send({message: 'Film not found', status: 404});
+        return res.status(200).json(game);
+    } catch (error) {
+        return res.status(500).send({error: 'Error retrieving film', status: 500});
+    }
+}
 
-//getFilmByTitle()
+exports.getFilmByDirector = async (req, res) => {
+    try {
+        const film = await Film.findOne({where: {director: req.params.director}});
+        if (film == null)
+            return res.status(404).send({message: 'Film not found', status: 404});
+        return res.status(200).json(game);
+    } catch (error) {
+        return res.status(500).send({error: 'Error retrieving film', status: 500});
+    }
+}
 
-//getFilmByDirector()
+exports.addFilm = async (req, res) => {
+    const {title, director, description, duration} = req.body;
+    try {
+        const film = await Film.create({title, director, description, duration});
+        await film.save();
+        return res.status(200).send({message: 'Film created', status: 200});
+    } catch (error) {
+        return res.status(500).send({error: 'Error creating film', status: 500});
+    }
+}
 
-//addFilm()
+exports.updateFilm = async (req, res) => {
+    const {title, director, description, duration} = req.body;
+    try {
+        const film = await Film.findByPk(req.params.id);
+        if (film == null)
+            return res.status(404).send({message: 'Film not found', status: 404});
+        film.title = title;
+        film.director = director;
+        film.description = description;
+        film.duration = duration;
+        await film.save();
+        return res.status(200).send({message: 'Film updated', status: 200});
+    } catch (error) {
+        return res.status(500).send({error: 'Error updating film', status: 500});
+    }
+}
 
-//updateFilm()
-
-//deleteFilmById()
+exports.removeFilmById = async (req, res) => {
+    try {
+        const film = await Film.findByPk(req.params.id);
+        if (film == null)
+            return res.status(404).send({message: 'Film not found', status: 404});
+        await film.destroy();
+    } catch (error) {
+        return res.status(500).send({error: 'Error deleting film', status: 500});
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const app = express();
 const sequelize = require('./database/connection');
-const associations = require('./models/associations');
 
-sequelize.sync({ force: true });
+const filmRoute = require('./routes/filmRoute');
+
+app.use(filmRoute);
+
+sequelize.sync({ force: true })
+    .then(() => {console.log("Connessione al Database effettuata")})
+    .catch((error) => {console.log("Connessione al Database fallita"+error.message)
+});
+
+app.get('/', (req, res) => {
+    res.send('Home page')
+});
+
+app.listen(3000, () => {
+    console.log('Server in ascolto nella porta 3000')
+});
