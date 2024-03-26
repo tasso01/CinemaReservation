7c62aeffb8a406ad96493fa3df1370fcbf973ce5
tasso01 <francescotasso16@gmail.com>
2024-03-26 15:48:24 +0100

    implemented sing up

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -21,10 +21,21 @@ exports.getUserById = async (req, res) => {
 }
 
 exports.register =  async (req, res) => {
-    //
-    const token = generateAccessToken({username: req.body.username});
-    res.json(token);
-    //
+    const {username, password} = req.body;
+    try {
+        const existingUser = User.findOne({where: {username: username}})
+        if (existingUser !== null) {
+            return res.status(400).json({message: "Username already used"})
+        }
+        const newUser = await User.create({username, password});
+        await newUser.save();
+        const token = generateAccessToken({username: req.body.username});
+        res.status(201).json(token);
+    } catch (error) {
+        console.log(error);
+        res.status(500);
+    }
+    
 }
 
 exports.login = async (req, res) => {
