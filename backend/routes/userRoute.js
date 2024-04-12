853a0967306b76679618483ecbbd689516c26989
tasso01 <francescotasso16@gmail.com>
2024-04-12 17:19:07 +0200

    fiexd user routes in backend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,8 +3,8 @@ const userController = require('../controller/userController');
 
 router.get('/users/allUsers', userController.getAllUsers);
 router.get('/users/userById/:id', userController.getUserById);
-router.post('/authentication/register', userController.register);
-router.post('/authentication/login', userController.login);
-router.post('/authentication/logout', userController.logout);
+router.post('/user/register', userController.register);
+router.post('/user/login', userController.login);
+router.post('/user/logout', userController.logout);
 
 module.exports = router;
