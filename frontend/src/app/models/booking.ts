3fb3d8351853d60135c379850f9e4c8b18abd71a
tasso01 <francescotasso16@gmail.com>
2024-04-12 16:16:19 +0200

    created model in frontend

---
+++ frontend/src/app/models/booking.ts
@@ -0,0 +1,15 @@
+export class Show{
+    id: number;
+    seats: number;
+    price: number;
+    userId: number;
+    showId: number;
+
+    constructor(id: number, seats: number, price: number, userId: number, showId: number){
+        this.id = id;
+        this.seats = seats;
+        this.price = price;
+        this.userId = userId;
+        this.showId = showId;
+    }
+}
--- frontend/src/app/models/show.ts
+++ frontend/src/app/models/show.ts
@@ -2,13 +2,15 @@ export class Show{
     id: number;
     date: Date;
     price: number;
+    freeSeats: number;
     hallId: number;
     filmId: number;
 
-    constructor(id: number, date: Date, price: number, hallId: number, filmId: number){
+    constructor(id: number, date: Date, price: number, freeSeats: number, hallId: number, filmId: number){
         this.id = id;
         this.date = date;
         this.price = price;
+        this.freeSeats = freeSeats;
         this.hallId = hallId;
         this.filmId = filmId;
     }
